3b1907bbcd44798d1faed593aa5dc0d264ca574c
“Elsa-zlt” <2903250242@qq.com>
2022-04-21 22:44:57 +0800

    commit

--- src/sort/InsertSort.java
+++ src/sort/InsertSort.java
@@ -8,6 +8,13 @@ public class InsertSort {
         for (int i = 0; i < a.length; i++) {
             System.out.println(a[i]);
         }
+
+        a = new int[]{3, 2, 1, 5, 4};
+        BinaryInsertSort(a,a.length);
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+
     }
 
     // 插入排序
@@ -23,7 +30,7 @@ public class InsertSort {
             int tmp = arr[i];
             int j;
 
-            for(j = i-1; j >= 0; j--){
+            for(j = i - 1; j >= 0; j--){
                 //如果比tmp大把值往后移动一位
                 if(arr[j] > tmp){
                     arr[j+1] = arr[j];
@@ -34,6 +41,33 @@ public class InsertSort {
             }
 
             arr[j+1] = tmp;
+        }
+    }
+
+    // 插入排序改进：二分插入排序
+    private static void BinaryInsertSort(int arr[], int len)
+    {
+        int key, left, right, middle;
+        for (int i = 1; i < len; i++)
+        {
+            key = a[i];
+            left = 0;
+            right = i - 1;
+            while (left <= right)
+            {
+                middle = (left + right)/2;
+                if (a[middle] > key)
+                    right = middle - 1;
+                else
+                    left = middle + 1;
+            }
+
+            for(int j=i - 1; j >= left; j--)
+            {
+                a[j+1] = a[j];
+            }
+
+            a[left] = key;
         }
     }
 
